c3b3b18b7353bfa9d3c9f2f837875d293e8c072f
Mahi Bhai <maheshlekhwani93@gmail.com>
2024-05-05 14:16:17 +0530

    added semicolon

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use('/users', usersRouter);
 
 app.get("/test", (_req, res) =>  {
   res.status(200).send("Hello world")
-})
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
